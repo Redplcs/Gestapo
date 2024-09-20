fd80e58386b19868a7fd971ad3712f9c58e71d8c
Redplcs <51379496+Redplcs@users.noreply.github.com>
2024-09-20 16:35:40 +0500

    Add server methods for starting, stopping, disposing

---
+++ src/Server/src/GestapoServer.cs
@@ -0,0 +1,19 @@
+﻿namespace CommandoTools.Gestapo.Server;
+
+public class GestapoServer : IServer
+{
+	public void Dispose()
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task StartAsync(CancellationToken cancellationToken = default)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task StopAsync(CancellationToken cancellationToken = default)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ src/Server/src/IServer.cs
@@ -0,0 +1,7 @@
+﻿namespace CommandoTools.Gestapo.Server;
+
+public interface IServer : IDisposable
+{
+	Task StartAsync(CancellationToken cancellationToken = default);
+	Task StopAsync(CancellationToken cancellationToken = default);
+}
