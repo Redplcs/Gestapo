d2a0c06c092e5461519abc93f6c1590b53def5c2
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-11-14 16:49:58 +0300

    Adapt server as hosted service

---
+++ src/Application/src/HostedServerAdapter.cs
@@ -0,0 +1,17 @@
+﻿using CommandoTools.Gestapo.Server;
+using Microsoft.Extensions.Hosting;
+
+namespace CommandoTools.Gestapo.Application;
+
+public class HostedServerAdapter(IServer server) : IHostedService
+{
+	public Task StartAsync(CancellationToken cancellationToken)
+	{
+		return server.StartAsync(cancellationToken);
+	}
+
+	public Task StopAsync(CancellationToken cancellationToken)
+	{
+		return server.StopAsync(cancellationToken);
+	}
+}
--- src/Application/src/Program.cs
+++ src/Application/src/Program.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using CommandoTools.Gestapo.Application;
+using CommandoTools.Gestapo.Server;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var host = Host.CreateApplicationBuilder(args);
+
+host.Services.AddSingleton<IServer, GestapoServer>();
+host.Services.AddHostedService<HostedServerAdapter>();
 
 await host.Build().RunAsync();
